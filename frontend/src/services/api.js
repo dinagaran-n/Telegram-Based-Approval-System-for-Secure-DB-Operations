7a06650c5acaf2be08d77b7fb9ef9b8a46024ee0
Dinagaran <dina71861@gmail.com>
2026-04-18 18:14:49 +0530

    feat: initialize axios API service with user and request endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API,
   headers: {
     'Content-Type': 'application/json'
   }
@@ -11,7 +11,7 @@ const api = axios.create({
 
 export const getRequests = () => api.get('/requests').then(res => res.data);
 export const getUsers = () => api.get('/users').then(res => res.data);
-export const createUser = (data) => api.post('/users', data).then(res => res.data);
+export const createUser = (data) => axios.post(`${API}/users`, { name: data.name, email: data.email }, { headers: { 'Content-Type': 'application/json' } }).then(res => res.data);
 export const updateUser = (id, data) => api.put(`/users/${id}`, data).then(res => res.data);
 export const deleteUser = (id) => api.delete(`/users/${id}`).then(res => res.data);
 
